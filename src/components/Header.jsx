da209e9220bb1c92931879a4d18ba2f5ee9ba58f
brunacarodrigues <brunacris.rodrigues@gmail.com>
2022-11-09 13:16:21 -0300

    requisito 4 e 5

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { getUser } from '../services/userAPI';
+import Loading from './Loading';
 
-export default class Header extends Component {
+class Header extends Component {
+  state = {
+    name: {},
+    loading: true,
+  };
+
+  async componentDidMount() {
+    const userName = await getUser();
+    this.setState({ name: userName.name, loading: false });
+  }
+
   render() {
+    const { name, loading } = this.state;
+    if (loading) return (<Loading />);
     return (
-      <div>Header</div>
+      <header data-testid="header-component">
+        {/* <div>{ loading && <Loading />}</div> pq não funciona? */}
+        <p data-testid="header-user-name">{ name }</p>
+        <Link to="/search" data-testid="link-to-search" />
+        <Link to="/favorites" data-testid="link-to-favorites" />
+        <Link to="/profile" data-testid="link-to-profile" />
+      </header>
     );
   }
 }
+
+export default Header;
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,9 +1,42 @@
 import React, { Component } from 'react';
+import Header from '../components/Header';
 
 class Search extends Component {
+  state = {
+    buttonDisabled: true,
+  };
+
+  buttonValidation = (event) => {
+    const minChars = 2;
+    const buttonValid = event.target.value.length >= minChars;
+    this.setState({ buttonDisabled: !buttonValid });
+  };
+
   render() {
+    const { buttonDisabled } = this.state;
+
     return (
-      <div data-testid="page-search">Search</div>
+      <div data-testid="page-search">
+        <Header />
+        <div className="search-container">
+          <label htmlFor="name-artist">
+            <input
+              type="text"
+              name="artist"
+              placeholder="nome do artista"
+              data-testid="search-artist-input"
+              onChange={ this.buttonValidation }
+            />
+            <button
+              type="button"
+              data-testid="search-artist-button"
+              disabled={ buttonDisabled }
+            >
+              Pesquisar
+            </button>
+          </label>
+        </div>
+      </div>
     );
   }
 }
